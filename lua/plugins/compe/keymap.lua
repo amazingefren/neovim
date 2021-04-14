009169f3d99f988b3e5a16dcbc9426c69be5b0a0
amazingefren <efrentheroker@hotmail.com>
2021-04-14 00:15:32 -0600

    readded lexima+compe requirement

--- lua/plugins/compe/keymap.lua
+++ lua/plugins/compe/keymap.lua
@@ -9,16 +9,14 @@
 -- inoremap <silent><expr> <C-d>     compe#scroll({ 'delta': -4 })
 -- ]]
 
---Apply.var({lexima_no_default_rules = "v:true"})
---vim.cmd[[
---call lexima#set_default_rules()
---]]
+Apply.var({lexima_no_default_rules = "v:true"})
+vim.cmd[[
+call lexima#set_default_rules()
+]]
 
 vim.api.nvim_set_keymap('i','<C-Space>' ,'compe#complete()' , {expr = true, noremap = true, silent=true})
 vim.api.nvim_set_keymap('i','<CR>' ,'compe#confirm(lexima#expand("<LT>CR>", "i"))' , {expr = true, noremap = true, silent=true})
 vim.api.nvim_set_keymap('i','<C-e>' ,'compe#close("<C-e>")' , {expr = true, noremap = true, silent=true})
--- vim.api.nvim_set_keymap('i','<C-f>' ,'compe#scroll({ "delta": +4 })' , {expr = true, noremap = true, silent=true})
--- vim.api.nvim_set_keymap('i','<C-d>' ,'compe#scroll({ "delta": -4 })' , {expr = true, noremap = true, silent=true})
 
 vim.api.nvim_set_keymap("i", "<Tab>", "v:lua.tab_complete()", {expr = true})
 vim.api.nvim_set_keymap("s", "<Tab>", "v:lua.tab_complete()", {expr = true})
