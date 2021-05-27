fb57197d8a0440296034b40d5641925311b00838
amazingefren <efrentheroker@hotmail.com>
2021-05-26 19:31:49 -0600

    blankline highlight + better vertical bar

--- lua/plugins/indentline/init.lua
+++ lua/plugins/indentline/init.lua
@@ -6,10 +6,18 @@ local vars = {
     -- indentLine_leadingSpaceEnabled = true,
     -- indent_blankline_space_char = '·',
     -- indent_blankline_space_char = '',
-    -- indent_blankline_char = '|',
-    indent_blankline_char = '',
-    indent_blankline_bufname_exclude = {'dashboard', 'NvimTree'}
+    indent_blankline_char = '│',
+    -- indent_blankline_char = '',
+    -- indent_blankline_char= '',
+    indent_blankline_bufname_exclude = {'dashboard', 'NvimTree'},
+    indent_blankline_use_treesitter = true,
 }
+
+
+
+vim.cmd[[
+    let g:indent_blankline_char_highlight_list = ['Operator']
+]]
 
 vim.wo.colorcolumn = "999" -- Artifact Fix for now
 
