92ef9fa49fb791ee6004988751a0c6b51fcc6602
amazingefren <efrentheroker@hotmail.com>
2021-06-24 17:05:41 -0600

    a usable galaxyline

--- lua/plug-galaxyline/init.lua
+++ lua/plug-galaxyline/init.lua
@@ -2,6 +2,8 @@ local gl = require("galaxyline")
 local gls = gl.section
 local condition = require("galaxyline.condition")
 
+gl.short_line_list = {'packager', 'NvimTree'}
+
 local checkwidth = function()
     local squeeze_width = vim.fn.winwidth(0) / 2
     if squeeze_width > 30 then
@@ -16,6 +18,23 @@ end
 
 local colors = require"plug-colorschemes/bogsterish".colors
 
+local mode_color = function()
+    local mode_colors = {
+        n = colors.blue,
+        i = colors.green,
+        c = colors.orange,
+        V = colors.purple,
+        [''] = colors.purple,
+        v = colors.purple,
+        R = colors.red
+    }
+    local color = mode_colors[vim.fn.mode()]
+    if color == nil then
+        color = colors.base3
+    end
+    return color
+end
+
 gls.left[0] = {
     ViMode = {
         provider = function()
@@ -29,6 +48,7 @@ gls.left[0] = {
                 R = "Replace"
             }
             local current_Mode = alias[vim.fn.mode()]
+            vim.api.nvim_command('hi GalaxyViMode guibg='..mode_color())
 
             if current_Mode == nil then
                 return "  Terminal "
@@ -36,14 +56,15 @@ gls.left[0] = {
                 return "  " .. current_Mode .. " "
             end
         end,
-        highlight = {colors.fg1, colors.base3},
+        highlight = {colors.base0, colors.base1, "bold"},
     }
 }
+
 gls.left[1] = {
     Prefix = {
         provider = spacing(1),
-        condition = require("galaxyline.provider_vcs").check_git_workspace,
-        highlight = {colors.base2, colors.base2},
+        condition = condition.check_git_workspace,
+        highlight = {colors.base1, colors.base1},
     }
 }
 
@@ -51,16 +72,16 @@ gls.left[2] = {
     GitBranch = {
         provider = "GitBranch",
         icon = ' ',
-        condition = require("galaxyline.provider_vcs").check_git_workspace,
-        highlight = {colors.fg1, colors.base2},
+        condition = condition.check_git_workspace,
+        highlight = {colors.fg1, colors.base1},
     },
 }
 gls.left[3] = {
     Prefix = {
         provider = spacing(1),
-        condition = require("galaxyline.provider_vcs").check_git_workspace,
-        highlight = {colors.base2, colors.base2},
-    }
+        condition = condition.check_git_workspace,
+        highlight = {colors.base1, colors.base1},
+    },
 }
 
 gls.left[4] = {
@@ -74,59 +95,69 @@ gls.left[4] = {
             },
         condition = checkwidth,
         highlight = {colors.fg1, colors.base1},
-        -- separator = " ",
-        -- separator_highlight = {colors.lightbg2, colors.statusline_bg}
+        separator = " ",
+        separator_highlight = {colors.base1, colors.base0}
     }
 }
 
 gls.left[5] = {
-    DiffAdd = {
-        provider = "DiffAdd",
-        condition = checkwidth,
-        icon = "+",
-        highlight = {colors.green, colors.base1}
+    FileName = {
+        provider = function()
+            local file = vim.fn.expand('%:t')
+            if vim.fn.empty(file) == 1 then return '' end
+
+            if vim.bo.modifiable == false then
+                vim.api.nvim_command('hi GalaxyFileName guifg='..colors.red)
+            end
+            return file .. ' '
+        end,
+        highlight = {colors.fg0, colors.base0},
+        -- condition = condition.buffer_not_empty and condition.hide_in_width,
+        condition = condition.buffer_not_empty and checkwidth
+        -- condition = checkwidth
     }
 }
 
 gls.left[6] = {
-    DiffModified = {
-        provider = "DiffModified",
+    DiffAdd = {
+        provider = "DiffAdd",
         condition = checkwidth,
-        icon = "~",
-        highlight = {colors.yellow, colors.base1}
+        icon = "+",
+        highlight = {colors.green, colors.base0, "bold"}
     }
 }
 
 gls.left[7] = {
-    DiffRemove = {
-        provider = "DiffRemove",
+    DiffModified = {
+        provider = "DiffModified",
         condition = checkwidth,
-        icon = "-",
-        highlight = {colors.red, colors.base1},
+        icon = "~",
+        highlight = {colors.yellow, colors.base0, "bold"}
     }
 }
 
 gls.left[8] = {
+    DiffRemove = {
+        provider = "DiffRemove",
+        condition = checkwidth,
+        icon = "-",
+        highlight = {colors.red, colors.base0, "bold"},
+    }
+}
+
+gls.left[9] = {
     fill_normal = {
         provider = function()
             return ""
         end,
-        highlight = {colors.fg1, colors.base0}
-    }
-}
-
-gls.mid[0] = {
-    FileName = {
-        provider = "FileName",
-        highlight = {colors.fg3, colors.base0},
-        condition = condition.buffer_not_empty and condition.hide_in_width,
-        -- condition = checkwidth
+        highlight = {colors.fg0, colors.base0}
     }
 }
 
 gls.right[0] = {
     LineColumn = {
-        provider = "LineColumn"
+        provider = "LineColumn",
+        highlight = {colors.fg0, colors.base0},
     }
 }
 
@@ -144,6 +175,56 @@ gls.right[1] = {
             local result, _ = math.modf((current_line / total_line) * 100)
             return "  " .. result .. "% "
         end,
-        highlight = {colors.green, colors.lightbg}
+        highlight = {colors.green, colors.base0}
+    }
+}
+
+gls.short_line_left[1] = {
+    FileName = {
+        provider = function()
+            local file = vim.fn.expand('%:t')
+            if vim.fn.empty(file) == 1 then return '' end
+
+            if vim.bo.modifiable == false then
+                vim.api.nvim_command('hi GalaxyFileName guifg='..colors.red)
+            end
+            return file .. ' '
+        end,
+        highlight = {colors.fg0, colors.base0},
+        condition = condition.buffer_not_empty and checkwidth
+    }
+}
+
+gls.short_line_right[1] = {
+    DiffAdd = {
+        provider = "DiffAdd",
+        condition = checkwidth,
+        icon = "+",
+        highlight = {colors.green, colors.base0, "bold"}
+    }
+}
+
+gls.short_line_right[2] = {
+    DiffModified = {
+        provider = "DiffModified",
+        condition = checkwidth,
+        icon = "~",
+        highlight = {colors.yellow, colors.base0, "bold"}
+    }
+}
+
+gls.short_line_right[3] = {
+    DiffRemove = {
+        provider = "DiffRemove",
+        condition = checkwidth,
+        icon = "-",
+        highlight = {colors.red, colors.base0, "bold"},
+    }
+}
+
+gls.short_line_right[4] = {
+    LineColumn = {
+        provider = "LineColumn",
+        highlight = {colors.fg0, colors.base0},
     }
 }
