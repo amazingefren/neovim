34f49764ae4821bcc5fe7daa302945bcda119fdc
amazingefren <efrentheroker@hotmail.com>
2021-07-27 01:49:34 -0600

    ToggleTerm Support, LazyGit FTW

--- lua/init-plug.lua
+++ lua/init-plug.lua
@@ -164,7 +164,6 @@ require('packer').startup(function(use)
     --= File Tree =--
     use { 'kyazdani42/nvim-tree.lua',
         -- See nvim-tree.lua LINE: #199->#201
-        -- event='VimEnter',
         config = function()
             require'plug-nvimtree'
         end
@@ -172,6 +171,24 @@ require('packer').startup(function(use)
 
     --= Formatting =--
     use {'mhartington/formatter.nvim', config=function() require'plug-lspconfig/format' end}
+
+    --= Toggle Term =--
+    use {"akinsho/nvim-toggleterm.lua",
+        config = function()
+          require'toggleterm'.setup{
+            direction = 'float'
+          }
+          local Terminal = require('toggleterm.terminal').Terminal
+          local lg = Terminal:new({cmd="lazygit", hidden=true})
+          -- vim.api.nvim_set_keymap('t','<Esc>', '<C-\\><C-n>', {noremap = true, silent = true})
+          vim.api.nvim_set_keymap('n',"<C-Space>", ':ToggleTerm<CR>', {noremap = true, silent = true})
+          vim.api.nvim_set_keymap('t',"<C-Space>", '<C-\\><C-n>:ToggleTerm<CR>', {noremap = true, silent = true})
+          function LG_TOGGLE()
+            lg:toggle()
+          end
+          vim.api.nvim_set_keymap('n',"<leader>lg", '<Cmd>lua LG_TOGGLE()<CR>', {noremap = true, silent = true})
+        end
+    }
 
     --= Devicons =--
     use 'kyazdani42/nvim-web-devicons'
