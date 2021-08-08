dbcd37a17f34cc8b75f69bba51a113d685f07add
amazingefren <efrentheroker@hotmail.com>
2021-08-07 23:01:00 -0600

    galaxyline colors exports doing it tomorrow

---
+++ lua/plug-colorschemes/dev-idk.lua
@@ -0,0 +1,19 @@
+-- Will be exported to galaxyline tomorrow when I have time
+-- TODO: marking for reminder
+local colors = {
+  b0 = '#0A0E1F',
+  b1 = '#131939',
+  b2 = '#202A60',
+  b3 = '#3D4B8F',
+  b4 = '#ACB3D2',
+  b5 = '#DEE0ED',
+  b8 = '#DB7070',
+  b9 = '#EB8B47',
+  ba = '#DBC970',
+  bb = '#82DB70',
+  bc = '#70DBA6',
+  bd = '#70C9DB',
+  be = '#9470DB'
+}
+
+return {colors}
