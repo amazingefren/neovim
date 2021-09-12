4b862f1e9c7974dd9e395e599780a9ae8ca658e4
amazingefren <efrentheroker@hotmail.com>
2021-09-11 19:07:42 -0600

    telescope hidden files

--- lua/plug-telescope/setup.lua
+++ lua/plug-telescope/setup.lua
@@ -29,7 +29,7 @@ require('telescope').setup{
       },
     },
     file_sorter =  require'telescope.sorters'.get_fuzzy_file,
-    file_ignore_patterns = {"node_modules", 'package-lock'},
+    file_ignore_patterns = {"node_modules/*", 'package-lock', '.git/*'},
     generic_sorter =  require'telescope.sorters'.get_generic_fuzzy_sorter,
     -- path_display = {
       -- "shorten",
