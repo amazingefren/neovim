e6c2d4078242f5f4f08b53d80eb37fbb1e2ce1be
amazingefren <efrentheroker@hotmail.com>
2021-09-13 17:00:49 -0600

    [fix] undotree path

--- after/plugin/undotree.lua
+++ after/plugin/undotree.lua
@@ -1,4 +1,4 @@
 vim.api.nvim_set_keymap("n", "<Leader>u", ":UndotreeToggle<CR>", {silent = false})
 
 vim.opt.undofile = true
-vim.opt.undodir = "$HOME/.undodir"
+vim.opt.undodir = os.getenv("HOME").."/.undodir"
